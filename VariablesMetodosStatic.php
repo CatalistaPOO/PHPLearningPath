44e5228d1daa97bb454f1bfb05649bc08ec50fae
CatalistaPOO <fgtpoo78@gmail.com>
2025-11-22 23:01:27 +0100

    style: Apply minor code formatting and add a test HTML comment.

--- VariablesMetodosStatic.php
+++ VariablesMetodosStatic.php
@@ -1,50 +1,61 @@
 <?
-class Estatico{
-    
+class Estatico
+{
+
     private static int $descuentoPromocional = 0;//pertenece únicamente a la clase     
-    
+
     public function __construct(
         private int $precio,
         private int $impuesto,
-    ){}
-        
-    public function getPrecio(){
+    ) {
+    }
+
+    public function getPrecio()
+    {
         return $this->precio;
     }
-    public function getImpuesto(){
+    public function getImpuesto()
+    {
         return $this->impuesto;
     }
-    public function setPrecio($precio){
+    public function setPrecio($precio)
+    {
         $this->precio = $precio;
     }
-    public function setImpuesto($impuesto){
+    public function setImpuesto($impuesto)
+    {
         $this->impuesto = $impuesto;
     }
 
-    public function getPrecioFinal(){
+    public function getPrecioFinal()
+    {
         self::aplicaPromocion();//llamamos metodo static (self::metodo)
-        $preciofinal =(int) (($this->getPrecio() + (($this->getImpuesto() / 100) * $this->getPrecio())) - (self::$descuentoPromocional /100 * $this->getPrecio()));//llamamos variable static (self::metodo)
+        $preciofinal = (int) (($this->getPrecio() + (($this->getImpuesto() / 100) * $this->getPrecio())) - (self::$descuentoPromocional / 100 * $this->getPrecio()));//llamamos variable static (self::metodo)
         return $preciofinal;
     }
 
-     //pertence solo a la clase(static)
-    private static function aplicaPromocion(){
+    //pertence solo a la clase(static)
+    private static function aplicaPromocion()
+    {
         $hoy = date("Y-m-d");//ojo, siempre dos cifras en mes y día
-        if( $hoy >= "2025-11-01" && $hoy <= "2025-11-12"){
+        if ($hoy >= "2025-11-01" && $hoy <= "2025-11-12") {
             self::$descuentoPromocional = 50;
         }
     }
 
-    public function __toString():string{
+    public function __toString(): string
+    {
         $precioCalculado = $this->getPrecioFinal();
         return "Precio de producto1: " . $this->getPrecio() . "\n"
-        . " Impuesto con valor: " . $this->getImpuesto() . "\n"
-        . "Precio final con descuentoPromocional con valor " .  self::$descuentoPromocional . ": " . $precioCalculado;
+            . " Impuesto con valor: " . $this->getImpuesto() . "\n"
+            . "Precio final con descuentoPromocional con valor " . self::$descuentoPromocional . ": " . $precioCalculado;
     }
 
 }
 
-$producto1=new Estatico(145,10);
+$producto1 = new Estatico(145, 10);
 echo $producto1;
 
 ?>
+
+<!-- Test GH -->
